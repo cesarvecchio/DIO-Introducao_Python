9292a5fda11793114f2bac28f250fa988b492d7d
cesarvecchio <cesar.vecchio@bandtec.com.br>
2020-05-14 13:17:37 -0300

    Criação e manipulação de classes e metodos, conceitos de orientação a objeto

---
+++ Introducao_Programacao_Python/aula07_calculadora1.py
@@ -0,0 +1,32 @@
+#declarar uma classe
+
+class Calculadora:
+    #funciona como um metodo construtor
+    def __init__(self, num1, num2):
+        self.valor_a = num1
+        self.valor_b = num2
+
+    #declarar um metodo
+    def soma(self):
+        return self.valor_a + self.valor_b
+
+    def subtracao(self):
+        return self.valor_a - self.valor_b
+
+    def multiplicaco(self):
+        return self.valor_a * self.valor_b
+
+    def divisao(self):
+        return self.valor_a / self.valor_b
+
+
+#instanciar uma classe
+calculadora = Calculadora(10, 2)
+
+print(calculadora.valor_a)
+print(calculadora.valor_b)
+
+print(calculadora.soma())
+print(calculadora.subtracao())
+print(calculadora.multiplicaco())
+print(calculadora.divisao())
---
+++ Introducao_Programacao_Python/aula07_calculadora2.py
@@ -0,0 +1,29 @@
+#declarar uma classe
+class Calculadora:
+    
+    #funciona como um metodo construtor
+    def __init__(self):
+        #faz com que não tenha necessidade de passar um valor como parametro no construtor
+        pass
+
+    #declarar um metodo
+    def soma(self, valor_a, valor_b):
+        return valor_a + valor_b
+
+    def subtracao(self, valor_a, valor_b):
+        return valor_a - valor_b
+
+    def multiplicaco(self, valor_a, valor_b):
+        return valor_a * valor_b
+
+    def divisao(self, valor_a, valor_b):
+        return valor_a / valor_b
+
+
+#instanciar uma classe
+calculadora = Calculadora()
+
+print(calculadora.soma(10, 3))
+print(calculadora.subtracao(19, 2))
+print(calculadora.multiplicaco(20, 4))
+print(calculadora.divisao(11, 55))
